9cd3f419ce3e3808744659954aa4dcafa5f495ac
Kevin Wu <kevin@kevinniuwu.com>
2024-12-22 23:46:23 -0500

    Add axum server

--- tinker-worker/src/main.rs
+++ tinker-worker/src/main.rs
@@ -1,19 +1,50 @@
 use tinker_worker::gen_img;
+use log::info;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
 
-    // return the image as a png
-    let start = std::time::Instant::now();
-    let data = gen_img().await.unwrap();
-    // write the image to a file
-    tokio::fs::write("data/test.png", data).await.unwrap();
-    println!("wrote image to data/test.png in {:?}", start.elapsed());
+    let app = axum::Router::new()
+        .route("/img", axum::routing::get(img_handler))
+        .route("/raw", axum::routing::get(raw_handler));
 
-    let start = std::time::Instant::now();
-    let data2 = gen_img().await.unwrap();
-    tokio::fs::write("data/test2.png", data2).await.unwrap();
-    println!("wrote image to data/test2.png in {:?}", start.elapsed());
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app.into_make_service()).await.unwrap();
+}
+
+async fn img_handler() -> axum::response::Response {
+    info!("img_handler");
+    match gen_img().await {
+        Ok(data) => axum::response::Response::builder()
+            .header("Content-Type", "image/png")
+            .body(axum::body::Body::from(data))
+            .unwrap(),
+        Err(e) => {
+            eprintln!("Error generating image: {}", e);
+            axum::response::Response::builder()
+                .status(500)
+                .body(axum::body::Body::from("Internal Server Error"))
+                .unwrap()
+        }
+    }
+}
+
+async fn raw_handler() -> axum::response::Response {
+    info!("raw_handler");
+    match tinker_worker::gen_raw().await {
+        Ok(data) => axum::response::Response::builder()
+            .header("Content-Type", "application/octet-stream") 
+            .body(axum::body::Body::from(data))
+            .unwrap(),
+        Err(e) => {
+            eprintln!("Error generating raw data: {}", e);
+            axum::response::Response::builder()
+                .status(500)
+                .body(axum::body::Body::from("Internal Server Error"))
+                .unwrap()
+        }
+    }
 }
